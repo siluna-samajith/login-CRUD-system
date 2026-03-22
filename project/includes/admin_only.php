675f84773a8e942fb2fcfbddeb864b4a7a727ec5
siluna-samajith <89127585+siluna-samajith@users.noreply.github.com>
2026-03-22 16:00:33 +0530

    Refactor admin-only access check in PHP file

--- project/includes/admin_only.php
+++ project/includes/admin_only.php
@@ -1,7 +1,7 @@
 <?php
 include("auth_check.php");
 
-// 🔥 block non-admins completely
+// block non-admins completely
 if ($_SESSION['role'] !== 'admin') {
     header("Location: ../user/home.php");
     exit();
